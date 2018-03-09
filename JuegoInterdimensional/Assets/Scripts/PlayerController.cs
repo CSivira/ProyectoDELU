252b59ba381c061a7a22ba133c0d983cf045d63f
Rixud <Rixud@users.noreply.github.com>
2018-03-08 20:32:46 -0400

    Player Movement y damage al EnemyLife

--- JuegoInterdimensional/Assets/Scripts/PlayerController.cs
+++ JuegoInterdimensional/Assets/Scripts/PlayerController.cs
@@ -8,17 +8,33 @@ public class PlayerController : MonoBehaviour {
     public float speed;
     private Animator anim;
     private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+    private bool faceLeft;
+    public BoxCollider2D weaponColl;
 
 	// Use this for initialization
 	void Start () {
         anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        faceLeft = false;
+        weaponColl.enabled = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-       move = new Vector2(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Vertical") * speed);
-       rb.velocity = move;
+        #region flip
+        if (Input.GetAxis("Horizontal")<0 && !faceLeft)
+        {
+            flip();
+        }else if (Input.GetAxis("Horizontal") > 0 && faceLeft)
+        {
+            flip();
+        }
+        #endregion
+        #region movement
+        move = new Vector2(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Vertical") * speed);
+        rb.velocity = move;
         if (Input.GetAxis("Horizontal")==0  && Input.GetAxis("Vertical") == 0)
         {
             anim.SetBool("run", false);
@@ -27,14 +43,49 @@ public class PlayerController : MonoBehaviour {
         {
             anim.SetBool("run", true);
         }
-        if(Input.GetAxis("Jump")!=0)
+        #endregion
+
+        if (Input.GetAxis("Jump") != 0)
         {
             anim.SetBool("attack", true);
+            weaponColl.enabled = true;
+            speed = 0f;
+
+        }
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("triggerEnter "+ other.gameObject.name);
+        if (other.gameObject.tag == "enemy")
+        {
+            other.gameObject.GetComponent<enemyLife>().damage(50);
+            
         }
     }
 
+    public void startAttack()
+    {
+       
+    }
     public void finishAttack()
     {
         anim.SetBool("attack", false);
+        
+        weaponColl.enabled = false;
+        speed = 3;
+    }
+
+    public void hurtStop()
+    {
+        anim.SetBool("hurt", false);
+    }
+    public void flip()
+    {
+        faceLeft = !faceLeft;
+        //sprite.flipX = faceLeft;
+        this.transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z); 
+       
     }
 }
---
+++ JuegoInterdimensional/Assets/Scripts/enemyLife.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyLife : MonoBehaviour {
+    public float life;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void damage(float amount)
+    {
+        Debug.Log("WeaponTrigger");
+        life -= amount;
+    }
+}
