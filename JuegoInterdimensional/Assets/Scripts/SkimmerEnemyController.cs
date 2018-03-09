a9dfad6ea1b6b3d900b1b960f8a1097c80dd4aca
Carlos Sivira <carlos.csivira@gmail.com>
2018-03-08 20:54:45 -0400

    Muestra del videojuego
    Se sobreescribio el script del player(de las escena juego, la de carlos), hay que reponerlo para mostrar como va a funcionar skimmer

--- JuegoInterdimensional/Assets/Scripts/SkimmerEnemyController.cs
+++ JuegoInterdimensional/Assets/Scripts/SkimmerEnemyController.cs
@@ -5,32 +5,40 @@ using UnityEngine;
 public class SkimmerEnemyController : MonoBehaviour {
 
 	public Transform target;
-	public Transform initialPosition;
 	public GameObject player;
 	public float speed = 2f;
 	public float maxDistance = 10f;
-	public float minDistance = 5f;
+	public float minDistance = 4f;
+	public GameObject projectile;
 
 	private Vector3 actualDistance;
 	private float actualMagnitude;
 	private Rigidbody2D rb2d;
+	private float burst = 5f;
 
 	// Use this for initialization
 	void Start () {
 		rb2d = GetComponent<Rigidbody2D> ();
+		projectile = GameObject.Find ("Projectile");
+		target.parent = null;
 	}
 
 	void FixedUpdate() {
 		actualDistance = transform.position - player.transform.position;
 		actualMagnitude = actualDistance.magnitude;
 		float fixedSpeed = speed * Time.deltaTime;
-		Debug.Log (actualDistance.normalized);
 
 		if (actualMagnitude < maxDistance && actualMagnitude > minDistance) {
-			//Dispara
+			InvokeRepeating("CreateProjectile",0f,burst);
 		}else if (actualMagnitude < maxDistance && actualMagnitude < minDistance){
+			CancelInvoke ("CreateProjectile");
 			target.position = actualDistance;
 			transform.position = Vector3.MoveTowards (transform.position,target.position,fixedSpeed);
+			Debug.Log (actualMagnitude);
 		}
+	}
+
+	void CreateProjectile() {
+		Instantiate (projectile,transform.position,Quaternion.identity);
 	}
 }
